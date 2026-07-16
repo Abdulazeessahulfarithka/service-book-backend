703d59fdb94479fd46b676873d531f18fb5f6a1a
“Abdul <kaazees31@gmail.com>
2026-07-16 15:52:56 +0530

    update

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,43 +1,43 @@
 import User from "../Model/User.js";
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-//register User
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, address, password } = req.body;
 
-export const registerUser = async(req,res)=>{
-    try{
-        const {name,email,address,password}=req.body
-        if(!name ||!email ||!address ||!password){
-            return res.status(400).json({
-                success:false,
-                message:"All fields are required"
-            })
-        }
- // check existing user
- const existingUser = await User.findOne({email})   
- 
-   if(existingUser){
-    return res.status(400).json({
-        success:false,
-        message:"User already exists"
-    })
-   } 
-   // hash password
- const hashedPassword = await bcrypt.hash(password, 10);
-//create user 
-const user = await User.create({
-    name,
-    email,
-    address,
-    password:hashedPassword
-})
-  res.status(201).json({
-    sucess:true,
-    message:"User registred sucessfully",
-    user,
-  })
+    if (!name || !email || !address || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
 
-}catch (error) {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      address,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
     res.status(500).json({
       success: false,
       message: error.message,
@@ -45,49 +45,56 @@ const user = await User.create({
   }
 };
 
-//Login User
-export const loginUser = async(req,res)=>{
-    try{
-        const {email,password} = req.body;
-      if(!email || !password){          
-        return res.status(404).json({
-            success:false,
-            message:"Email and password are required"
-        })
-      }                 
-     const user = await User.findOne({email})
-  if(!user){
-    return res.status(404).json({
-        sucess:false,
-        message:"User not found"
-    })
-  }
-   const isMatch = await bcrypt.compare(password,user.password)
+// Login User
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-   if(!isMatch){
-    return res.status(401).json({
-        sucess:false,
-        message:"invalid password"
-    })
-   }
- const token =jwt.sign(
-    {id:user._id},
-    process.env.JWT_SECRET,
-    {expiresIn:"7d"}
- )
-  res.status(200).json({
-    sucess:false,
-    message:"Login successful",
-    user:{
-        id:user._id,
-        name:user.name,
-        email:user.email,
-        address:user.address
-    },
-    token,
-  })
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
 
-    }catch (error) {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
     res.status(500).json({
       success: false,
       message: error.message,
